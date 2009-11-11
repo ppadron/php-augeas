995c36d0111cc7afde91dc70df0502f377b2a4dc
Pedro Padron <ppadron@w3p.com.br>
2009-11-11 19:37:34 -0200

    first commit

---
+++ augeas.c
@@ -0,0 +1,197 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2007 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id: header,v 1.16.2.1.2.1 2007/01/01 19:32:09 iliaa Exp $ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "augeas.h"
+#include "php_augeas.h"
+
+/* If you declare any globals in php_augeas.h uncomment this:
+ZEND_DECLARE_MODULE_GLOBALS(augeas)
+*/
+
+/* True global resources - no need for thread safety here */
+static int le_augeas;
+
+/* {{{ augeas_functions[]
+ *
+ * Every user visible function must have an entry in augeas_functions[].
+ */
+function_entry augeas_functions[] = {
+    PHP_FE(augeas_init,  NULL)
+    PHP_FE(augeas_get,   NULL)
+    PHP_FE(augeas_match, NULL)
+	{NULL, NULL, NULL}	/* Must be the last line in augeas_functions[] */
+};
+/* }}} */
+
+/* {{{ augeas_module_entry
+ */
+zend_module_entry augeas_module_entry = {
+	STANDARD_MODULE_HEADER,
+	"augeas",
+	augeas_functions,
+	PHP_MINIT(augeas),
+	PHP_MSHUTDOWN(augeas),
+	NULL,
+	NULL,
+	PHP_MINFO(augeas),
+	"0.1", /* Replace with version number for your extension */
+	STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_AUGEAS
+ZEND_GET_MODULE(augeas)
+#endif
+
+
+/* {{{ PHP_MINIT_FUNCTION
+ */
+PHP_MINIT_FUNCTION(augeas)
+{
+    le_augeas = zend_register_list_destructors_ex(NULL, NULL, PHP_AUGEAS_RESOURCE_NAME, module_number); 
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MSHUTDOWN_FUNCTION
+ */
+PHP_MSHUTDOWN_FUNCTION(augeas)
+{
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION
+ */
+PHP_MINFO_FUNCTION(augeas)
+{
+	php_info_print_table_start();
+	php_info_print_table_header(2, "augeas support", "enabled");
+	php_info_print_table_end();
+
+}
+/* }}} */
+
+PHP_FUNCTION(augeas_init) {
+
+  char *root;
+  char *loadpath;
+  int root_len, loadpath_len;
+  long flags;
+  php_augeas *aug;
+
+  if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|sl", &root, &root_len, &loadpath, &loadpath_len, &flags) == FAILURE) {
+    RETURN_FALSE;
+  }
+
+  aug = emalloc(sizeof(php_augeas));
+  aug->augeas = aug_init(root, "", 0);
+
+  ZEND_REGISTER_RESOURCE(return_value, aug, le_augeas);  
+
+}
+
+PHP_FUNCTION(augeas_get) {
+
+    php_augeas *aug;
+    char *path;
+    int path_len;
+    signed int retval;
+    char *value;
+    zval *zaug;
+   
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rs", &zaug, &path, &path_len) == FAILURE) {
+        RETURN_FALSE;
+    }
+
+    if (path_len < 1) {
+        RETURN_FALSE;
+    }   
+
+    aug = (php_augeas *) zend_fetch_resource(&zaug TSRMLS_CC, -1, PHP_AUGEAS_RESOURCE_NAME, NULL, 1, le_augeas);
+
+    retval = aug_get(aug->augeas, path, &value);
+
+    php_printf("%d", retval);
+
+    switch (retval) {
+        case 0:
+            RETURN_NULL();
+            break;
+        case 1:
+            RETURN_STRING(value, 1);
+            efree(value);
+            break;
+        default:
+            RETURN_FALSE;
+            break;
+    }
+    
+}
+
+
+
+PHP_FUNCTION(augeas_match) {
+
+    php_augeas *aug;
+    zval *zaug;
+    char *path;
+    char **matches;
+    int path_len, retval;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rs", &zaug, &path, &path_len) == FAILURE) {
+        RETURN_FALSE;
+    }
+
+    aug = (php_augeas *) zend_fetch_resource(&zaug TSRMLS_CC, -1, PHP_AUGEAS_RESOURCE_NAME, NULL, 1, le_augeas);
+
+    retval = aug_match(aug->augeas, path, &matches);
+
+    switch (retval) { 
+        case 0:
+            RETURN_NULL();
+            break;
+        case 1:
+            char *value;
+            aug_get(aug->augeas, path, &value);
+            RETURN_STRING(value, 1);
+            break;
+        default:
+            break;
+        }
+
+}
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
---
+++ config.m4
@@ -0,0 +1,39 @@
+dnl
+dnl $Id: config.m4,v 1.5.4.1 2006/12/23 17:58:47 derick Exp $
+dnl
+
+PHP_ARG_WITH(augeas,for AUGEAS support,
+[  --with-augeas[=DIR]       Include AUGEAS support])
+
+if test "$PHP_AUGEAS" != "no"; then
+
+  if test "$PHP_AUGEAS" != "yes"; then
+    AUGEAS_SEARCH_DIRS=$PHP_AUGEAS
+  else
+    AUGEAS_SEARCH_DIRS="/usr/local /usr"
+  fi
+
+  for i in $AUGEAS_SEARCH_DIRS; do
+    if test -f $i/include/augeas/augeas.h; then
+      AUGEAS_DIR=$i
+      AUGEAS_INCDIR=$i/include/augeas
+    elif test -f $i/include/augeas.h; then
+      AUGEAS_DIR=$i
+      AUGEAS_INCDIR=$i/include
+    fi
+  done
+
+  if test -z "$AUGEAS_DIR"; then
+    AC_MSG_ERROR(Cannot find libaugeas)
+  fi
+
+  AUGEAS_LIBDIR=$TIDY_DIR/$PHP_LIBDIR
+
+  PHP_ADD_LIBRARY_WITH_PATH(augeas, $AUGEAS_LIBDIR, AUGEAS_SHARED_LIBADD)
+  PHP_ADD_INCLUDE($AUGEAS_INCDIR)
+
+
+  PHP_NEW_EXTENSION(augeas, augeas.c, $ext_shared)
+  PHP_SUBST(AUGEAS_SHARED_LIBADD)
+  AC_DEFINE(HAVE_AUGEAS,1,[ ])
+fi
