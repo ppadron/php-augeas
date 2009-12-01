d0da99a71ff6994485f7fc980fffca08231804b5
Pedro Padron <ppadron@w3p.com.br>
2009-12-01 11:49:28 -0200

    spaces instead of tabs

--- tests/AugeasMatchTest.php
+++ tests/AugeasMatchTest.php
@@ -7,24 +7,24 @@ class AugeasMatchTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-	$this->augeas = new Augeas(dirname(__FILE__) . "/root");
+        $this->augeas = new Augeas(dirname(__FILE__) . "/root");
     }
 
     public function testShouldReturnEmptyArrayIfNoMatch()
     {
-	$this->assertEquals(array(), $this->augeas->match("/files/non_existent_directory"));
+        $this->assertEquals(array(), $this->augeas->match("/files/non_existent_directory"));
     }
 
     public function testShoulReturnArrayWithMatches()
     {
-	$expectedArray = array("/files/etc/hosts");
-	$this->assertEquals($expectedArray, $this->augeas->match("/files/etc/*"));
+        $expectedArray = array("/files/etc/hosts");
+        $this->assertEquals($expectedArray, $this->augeas->match("/files/etc/*"));
     }
 
     public function testShouldThrowExceptionIfPathIsInvalid()
     {
-	$this->expectedException = "AugeasException";
-	$this->augeas->match("OMG TOTALLY INVALID PATH");
+        $this->expectedException = "AugeasException";
+        $this->augeas->match("OMG TOTALLY INVALID PATH");
     }
 
 }
