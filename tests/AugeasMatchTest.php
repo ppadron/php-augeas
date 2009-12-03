441ac6ab6676d085ae15e775131c93434d693ae0
Pedro Padron <ppadron@w3p.com.br>
2009-12-03 02:15:21 -0200

    feature request: Augeas::match() should return assoc array with nodes => values

--- tests/AugeasMatchTest.php
+++ tests/AugeasMatchTest.php
@@ -17,8 +17,21 @@ class AugeasMatchTest extends PHPUnit_Framework_TestCase
 
     public function testShoulReturnArrayWithMatches()
     {
-        $expectedArray = array("/files/etc/hosts");
+        $expectedArray = array("/files/etc/hosts" => null);
         $this->assertEquals($expectedArray, $this->augeas->match("/files/etc/*"));
+    }
+
+    public function testShouldReturnArrayWithNodesAndValues()
+    {
+        $expectedArray = array(
+            "/files/etc/hosts/1/ipaddr"    => "127.0.0.1",
+            "/files/etc/hosts/1/canonical" => "localhost",
+            "/files/etc/hosts/1/alias"     => "localhost.localdomain",
+        );
+
+        $matches = $this->augeas->match("/files/etc/hosts/1/*");
+
+        $this->assertEquals($expectedArray, $matches);
     }
 
     public function testShouldThrowExceptionIfPathIsInvalid()
